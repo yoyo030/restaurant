ebba4469af01d9583d427af5c40e8732f48cbe9c
yoyo030 <jennifer77christian@gmail.com>
2022-06-14 11:25:04 +0800

    Create project. HomePage is complete.

---
+++ app.js
@@ -0,0 +1,24 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 5000
+
+// setting template engine
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')//相對路徑會去路徑找文件
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+    res.render('index',{restaurants : restaurantList.results})
+})
+
+
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
